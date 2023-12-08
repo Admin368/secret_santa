f998c024e91971bf1f856fdbf4e42d70ba5761ab
Admin368 <58757588+Admin368@users.noreply.github.com>
2023-12-08 17:34:32 +0800

    grinch updates

--- src/pages/revelio/grinch.tsx
+++ src/pages/revelio/grinch.tsx
@@ -1,41 +1,66 @@
+import { LoadingOutlined } from "@ant-design/icons";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import React from "react";
+import { api } from "~/utils/api";
 
-export default function revelio() {
-    function TextDisplay() {
-        const [text, setText] = useState(revealing[0]);
+interface PropsTextDisplay {
+  name: string;
+}
+function TextDisplay(props: PropsTextDisplay) {
+  const revealing = [
+    "Welcome,You are about to discover the identity of the person you will be gifting this year.",
+    "The moment of truth has arrived!Are you ready to find out who your Secret Santa is ? ",
+    `You have been chosen to be the Secret Santa for`,
+    props.name,
+  ];
+  const [text, setText] = useState(revealing[0]);
 
-        let index = 0;
+  let index = 0;
 
-        function changeText(): void {
-            setText(revealing[index]);
+  function changeText(): void {
+    setText(revealing[index]);
 
-            index++;
-            if (index >= revealing.length) {
-                index = 0;
-            }
-        }
-
-        useEffect(() => {
-            const timer = setInterval(changeText, 4000);
-            return () => clearInterval(timer);
-        }, []);
-
-        return <p className=" text-white font-bold text-2xl py-2.5">{text}</p>;
+    index++;
+    if (index >= revealing.length) {
+      index = 0;
     }
+  }
 
+  useEffect(() => {
+    const timer = setInterval(changeText, 3000);
+    return () => clearInterval(timer);
+  }, []);
 
-
-
-    const revealing = [
-        "Welcome,You are about to discover the identity of the person you will be gifting this year.",
-        "The moment of truth has arrived!Are you ready to find out who your Secret Santa is ? ",
-        "You have been chosen to be the Secret Santa for",]
-
-
-    return (
-        <div className=" min-h-screen container text-white text-center flex flex-col justify-center items-center">
-            {TextDisplay()}
-        </div>
-    )
+  return <p className=" py-2.5 text-2xl font-bold text-white">{text}</p>;
+}
+export default function revelio() {
+  const router = useRouter();
+  const id = router.query.id as string;
+  const santa = api.group.member_get.useQuery(
+    { id: id },
+    { enabled: id ? true : false, staleTime: 0 },
+  );
+  const receiver = api.group.member_get_my_receiver.useMutation();
+  useEffect(() => {
+    if (santa.data && !receiver.data?.receiver_name) {
+      receiver
+        .mutateAsync({ id: santa.data.id })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+  }, [id, receiver.data, santa.data]);
+  return (
+    <div className=" container flex min-h-screen flex-col items-center justify-center text-center text-white">
+      {receiver.data?.receiver_name ? (
+        <TextDisplay name={receiver.data?.receiver_name} />
+      ) : (
+        <LoadingOutlined />
+      )}
+    </div>
+  );
 }
